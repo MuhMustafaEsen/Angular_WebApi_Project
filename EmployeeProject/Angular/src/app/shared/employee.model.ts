b66aaf3764527215e0f05b61ccebb0e2aae7b533
mustafa esen <esenmustafa42@gmail.com>
2020-03-22 02:19:37 +0300

    Angular Project

---
+++ EmployeeProject/Angular/src/app/shared/employee.model.ts
@@ -0,0 +1,16 @@
+export class Employee {
+
+    EmployeeID : number;
+    FullName :string;
+    EMPCode :string;
+    Mobile:string;
+    Position:string;
+}
+export class User {
+
+    UserID : number;
+    Fullname : string;
+    Phone : string;
+    Email : string;
+    Tagname : string;
+}
